12f66e5cba78491c077749ec5a3f00c52b107c0c
raksa <eng.raksa@gmail.com>
2018-11-30 10:10:31 +0700

    add auth

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,7 @@ if (\File::isDirectory($component_path)) {
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
