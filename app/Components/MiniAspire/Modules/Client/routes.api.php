b970caa4867ec79711f19678ba26c2731854b486
raksa <eng.raksa@gmail.com>
2018-11-30 14:08:00 +0700

    add password auth

--- app/Components/MiniAspire/Modules/Client/routes.api.php
+++ app/Components/MiniAspire/Modules/Client/routes.api.php
@@ -3,7 +3,7 @@
  * Author: Raksa Eng
  */
 
-Route::group(['middleware' => ['auth.api.once'], 'prefix' => 'v1'], function () {
+Route::group(['middleware' => ['auth:api'], 'prefix' => 'v1'], function () {
     $controller = "\App\Components\MiniAspire\Modules\Client\ClientController";
     Route::post('/clients/create', $controller . '@apiCreateClient')->name('api.clients.create');
     Route::post('/clients/get/{id?}', $controller . '@apiGetClient')->name('api.clients.get');
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,8 @@
                     @endif
 
                     You are logged in!
+
+                    <div id="app"></div>
                 </div>
             </div>
         </div>
